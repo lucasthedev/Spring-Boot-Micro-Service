385b567834e6da2e698691a3f8b1bad5ed822bae
Lucas Pereira <lucas.pereira@duotecnologia.com.br>
2020-06-09 13:34:25 -0300

    nova implmentacoes

--- src/main/java/com/projuris/webservice/demo/ProdutoController.java
+++ src/main/java/com/projuris/webservice/demo/ProdutoController.java
@@ -1,11 +1,17 @@
 package com.projuris.webservice.demo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ProdutoController {
@@ -20,6 +26,15 @@ public class ProdutoController {
 		List<Produto> produtos = produtoReposiory.findAll();
 
 		return produtos;
+	}
+	
+	
+	
+	@PostMapping("/cadastrar")
+	@ResponseBody
+	public Produto cadastrarProduto(@Validated @RequestBody Produto produto) {		
+		
+		return this.produtoReposiory.save(produto);
 	}
 
 }
--- src/main/java/com/projuris/webservice/demo/ProdutoRepository.java
+++ src/main/java/com/projuris/webservice/demo/ProdutoRepository.java
@@ -1,7 +1,11 @@
 package com.projuris.webservice.demo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	
 	
 }
