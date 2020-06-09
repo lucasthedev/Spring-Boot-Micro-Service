25ff5dbf446d07559033ff6923ea5c9217ef9c2f
Lucas Pereira <lucas.pereira@duotecnologia.com.br>
2020-06-09 11:24:41 -0300

    config do banco

--- src/main/java/com/projuris/webservice/demo/Produto.java
+++ src/main/java/com/projuris/webservice/demo/Produto.java
@@ -1,7 +1,14 @@
 package com.projuris.webservice.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
 	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String nome;
 	
