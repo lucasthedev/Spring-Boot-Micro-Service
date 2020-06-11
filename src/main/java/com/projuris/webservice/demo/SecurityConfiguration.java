27c657772f45f3c5bcd8a35a0c34dada0e8ba798
Lucas Pereira <lucas.pereira@duotecnologia.com.br>
2020-06-11 14:06:24 -0300

    Criado classe usuario e perfil para fazer a autenticação de suário para métodos POST,PUT, DELETE

--- src/main/java/com/projuris/webservice/demo/SecurityConfiguration.java
+++ src/main/java/com/projuris/webservice/demo/SecurityConfiguration.java
@@ -21,12 +21,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	// authorization configure
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET,"/listar").permitAll()
-			.antMatchers(HttpMethod.GET,"/buscar/*").permitAll()
-			.antMatchers(HttpMethod.GET,"/buscarpornome").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/listar").permitAll()
+				.antMatchers(HttpMethod.GET, "/buscar/*").permitAll().antMatchers(HttpMethod.GET, "/buscarpornome")
+				.permitAll().anyRequest().authenticated()
+				.and().formLogin();
 
-		
 	}
 
 	// static resources configuration
