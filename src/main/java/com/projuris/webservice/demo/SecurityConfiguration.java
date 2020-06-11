4c19d7cffdb17f91e8c4dc4182766f5a1cd84792
Lucas Pereira <lucas.pereira@duotecnologia.com.br>
2020-06-11 11:25:42 -0300

    Liberando acesso total para métodos de consulta

--- src/main/java/com/projuris/webservice/demo/SecurityConfiguration.java
+++ src/main/java/com/projuris/webservice/demo/SecurityConfiguration.java
@@ -1,13 +1,38 @@
 package com.projuris.webservice.demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
-	
+
+	// authenticate configure
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+	}
+
+	// authorization configure
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET,"/listar").permitAll()
+			.antMatchers(HttpMethod.GET,"/buscar/*").permitAll()
+			.antMatchers(HttpMethod.GET,"/buscarpornome").permitAll();
+
+		
+	}
+
+	// static resources configuration
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+
+	}
 
 }
